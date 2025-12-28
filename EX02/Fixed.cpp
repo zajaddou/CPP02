4c531956f4b6f5e5fdc4a2326bb40a63ead8f12f
Zakaria Ajaddou <zajaddou@e3r11p4.1337.ma>
2025-12-28 09:59:56 +0100

    cpp02

--- EX02/Fixed.cpp
+++ EX02/Fixed.cpp
@@ -127,27 +127,27 @@ Fixed   Fixed::operator++(int)
 const   Fixed   &Fixed::max(const Fixed &a, const Fixed &b)
 {
     if (a > b)
-        return (b);
-    return (a);
+        return (a);
+    return (b);
 };
 
 const   Fixed   &Fixed::min(const Fixed &a, const Fixed &b)
 {
     if (a < b)
-        return (b);
-    return (a);
+        return (a);
+    return (b);
 };
 
 Fixed   &Fixed::max(Fixed &a, Fixed &b)
 {
     if (a > b)
-        return (b);
-    return (a);
+        return (a);
+    return (b);
 };
 
 Fixed   &Fixed::min(Fixed &a, Fixed &b)
 {
     if (a < b)
-        return (b);
-    return (a);
+        return (a);
+    return (b);
 };
